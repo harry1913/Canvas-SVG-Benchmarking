250c8667d37716257bb01da57e817ceab062c958
Harry <a1689938@student.adelaide.edu.au>
2018-06-10 17:23:33 +0930

    upload files

---
+++ benchmark/src/main/java/com/bench/web/WebController.java
@@ -0,0 +1,18 @@
+package com.bench.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class WebController {
+	
+	@RequestMapping("/")
+	public String home(Model model)
+	{
+		return "redirect:/main.html";
+	}
+	
+}
